45fbc4745a089e60e72b37b9027d5eaf0b6e3226
John Ceniza <jc@johnceniza.com>
2015-06-26 01:14:36 -0400

    first commit. working color changer with sliders

--- ColorMakerChallenge/ViewController.swift
+++ ColorMakerChallenge/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var squareColorView: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //initalize squareColorView as black
+        self.redSlider.value = 0
+        self.greenSlider.value = 0
+        self.blueSlider.value = 0
+        
+        updateSquareColorView()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sliderChanged(sender: UISlider) {
+        
+        //if else statement to determine which slider value is being changed
+        if sender == redSlider {
+            redSlider.value = sender.value
+        } else if sender == greenSlider {
+            greenSlider.value = sender.value
+        } else if sender == blueSlider {
+            blueSlider.value = sender.value
+        } else {
+            //This shouldn't happen. Like. Ever.
+        }
+        
+        updateSquareColorView()
     }
-
-
+    
+    //function to change color of view using slider values
+    func updateSquareColorView() {
+        
+        let r: CGFloat = CGFloat(self.redSlider.value)
+        let g: CGFloat = CGFloat(self.greenSlider.value)
+        let b: CGFloat = CGFloat(self.blueSlider.value)
+        
+        squareColorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
+    
 }
 
